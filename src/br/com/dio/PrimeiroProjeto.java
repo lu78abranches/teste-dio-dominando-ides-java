7919cddbd132ce75b04b9f54bc6c6ac2449774d3
lu78abranches <luisabranches.surf@hotmail.com>
2022-05-09 08:02:42 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroProjeto.java
@@ -0,0 +1,29 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livro = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		
+		
+		
+		
+		//System.out.println("Hello World");
+
+	}
+	
+	
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
